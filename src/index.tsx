0255554a98c86709da0395fd51a3c24438ee136b
miyabi-satoh <miyabi.satoh@gmail.com>
2022-03-04 01:02:49 +0900

    主要コード実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </CookiesProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
